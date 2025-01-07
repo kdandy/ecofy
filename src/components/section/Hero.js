40d1076345d15bf5e7823d313c5950700997df18
Alif Wibowo <alif.likes.coding@gmail.com>
2025-01-07 22:27:27 +0700

    Change hero text alignment

--- src/components/section/Hero.js
+++ src/components/section/Hero.js
@@ -5,7 +5,7 @@ import { Button } from "../ui/button";
 const Hero = () => {
   return (
     <div className="hero relative h-screen text-center bg-cover bg-center">
-      <CustomContainer className="text-left pt-[30vh]">
+      <CustomContainer className="text-left pt-[20vh]">
         <h1 className=" text-4xl sm:text-6xl font-bold max-w-[250px] sm:max-w-[500px] sm:leading-[75px]">
           Menghubungkan Komunitas untuk Masa Depan Hijau
         </h1>
