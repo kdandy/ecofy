65dcee1a0784cd7defa3051658df8bc86c4c2319
Alif Wibowo <alif.likes.coding@gmail.com>
2025-01-13 06:24:52 +0700

    Make community responsive

--- src/components/section/Community.js
+++ src/components/section/Community.js
@@ -7,17 +7,17 @@ const Community = () => {
   return (
     <div className="text-lora text-text mb-[15vh] relative">
       <CustomContainer className="flex relative text-center flex-col items-center justify-center">
-        <h1 className="text-4xl max-w-[770px] mb-16">
+        <h1 className="text-3xl sm:text-4xl max-w-[770px] mb-16">
           Join our community for volunteering and be a part of positive change
           in the world. with over:
         </h1>
-        <h1 className="text-9xl mb-12">125,259</h1>
+        <h1 className="text-6xl sm:text-9xl mb-12">125,259</h1>
         <div className="flex flex-col items-center gap-5">
-          <h3 className="text-2xl font-medium">People are already joining</h3>
+          <h3 className="text-md sm:text-2xl font-medium">People are already joining</h3>
           <div className="flex md:flex-row flex-col justify-center items-center z-20">
             <Link
               href="https://calculator.ecofy.dandy.my.id/"
-              className="capitalize bg-primary hover:bg-primary-foreground text-2xl py-3 px-10 rounded-full transition ease-in-out duration-300"
+              className="capitalize bg-primary hover:bg-primary-foreground text-md sm:text-2xl py-3 px-10 rounded-full transition ease-in-out duration-300"
             >
               <button>Yes I want to join our community</button>1
             </Link>
